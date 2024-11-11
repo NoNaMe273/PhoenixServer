81e8003bb4050e75528cbb5d1226fa0c59127d64
admin <mogilevzeka@gmail.com>
2024-11-12 01:38:50 +0500

    SQL

--- src/main/java/org/MogilevEvgeniy/PhoenixServer/Function.java
+++ src/main/java/org/MogilevEvgeniy/PhoenixServer/Function.java
@@ -8,10 +8,8 @@ public class Function {
     public static int createNewUser(String login, String password) {
         int result;
         try {
-            SQLConnect.connection("usersDB");
             SQLConnect.createDateBase("CREATE TABLE if not exists 'users' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'login' text, 'password' text);");
             SQLConnect.writeDateBase("INSERT INTO 'users' ('login', 'password') VALUES ('" + login + "', '" + password + "'); ");
-            SQLConnect.closeDateBase(0);
             result = 1;
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
@@ -22,9 +20,7 @@ public class Function {
     }
 
     public static String login(String login, String password) throws ClassNotFoundException, SQLException {
-        SQLConnect.connection("usersDB");
         String tempPassword = SQLConnect.readDateBase(login, "users");
-        SQLConnect.closeDateBase(2);
         int result;
         if (tempPassword.equals(password)) result = 1;
         else result = 0;
@@ -34,27 +30,20 @@ public class Function {
     public static int createNewDialog(String from, String to, String text) throws SQLException, ClassNotFoundException {
         int result;
         Date date = new Date();
-
-        SQLConnect.connection("Message");
         SQLConnect.createDateBase("CREATE TABLE if not exists 'dialogID' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'first' text, 'second' text);");
         result = SQLConnect.gettingIdDialog(from, to);
         if (result == 0) {
             SQLConnect.writeDateBase("INSERT INTO 'dialogID' ('first', 'second') VALUES ('" + from + "', '" + to + "'); ");
         }
         result = SQLConnect.gettingIdDialog(from, to);
-        SQLConnect.closeDateBase(1);
-        SQLConnect.connection("Message");
         SQLConnect.createDateBase("CREATE TABLE if not exists 'dialog' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'IDDialog' int, 'datetime' NOW, 'text' text, 'from' text);");
         SQLConnect.writeDateBase("INSERT INTO 'dialog' ('IDDialog', 'text', 'datetime', 'from') VALUES ('" + result + "', '" + text + "', '" + date + "', '" + from + "'); ");
-        SQLConnect.closeDateBase(1);
         return 1;
     }
 
     public static String getDialogNames(String login) throws SQLException, ClassNotFoundException {
-        SQLConnect.connection("Message");
         SQLConnect.createDateBase("CREATE TABLE if not exists 'dialogID' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'first' text, 'second' text);");
         String[] result = SQLConnect.gettingDialogNames(login).toArray(new String[0]);
-        SQLConnect.closeDateBase(2);
         StringBuilder tempResult;
         if (result.length > 1) {
             tempResult = new StringBuilder(result[0]);
@@ -68,12 +57,10 @@ public class Function {
     public static String getMessagesDialog(String login, String to) throws SQLException, ClassNotFoundException {
         int id = 0;
 
-        SQLConnect.connection("Message");
         SQLConnect.createDateBase("CREATE TABLE if not exists 'dialogID' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'first' text, 'second' text);");
         id = SQLConnect.gettingIdDialog(login, to);
         SQLConnect.createDateBase("CREATE TABLE if not exists 'dialog' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'IDDialog' int, 'datetime' NOW, 'text' text, 'from' text);");
         String[] result = SQLConnect.gettingDialog(id).toArray(new String[0]);
-        SQLConnect.closeDateBase(2);
 
         StringBuilder tempResult;
         if (result.length > 1) {
