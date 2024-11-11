c57ca463f0f9f4756394288496c28c25b9f2f4c9
admin <mogilevzeka@gmail.com>
2024-11-12 01:12:28 +0500

    SQL

--- src/main/java/org/MogilevEvgeniy/PhoenixServer/PhoenixServerApplication.java
+++ src/main/java/org/MogilevEvgeniy/PhoenixServer/PhoenixServerApplication.java
@@ -3,10 +3,16 @@ package org.MogilevEvgeniy.PhoenixServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class PhoenixServerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		SQLConnect.connection("DB");
+		SQLConnect.createDateBase("CREATE TABLE if not exists 'users' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'login' text, 'password' text);");
+		SQLConnect.createDateBase("CREATE TABLE if not exists 'dialogID' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'first' text, 'second' text);");
+		SQLConnect.createDateBase("CREATE TABLE if not exists 'dialog' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'IDDialog' int, 'datetime' text, 'text' text, 'from' text);");
 		SpringApplication.run(PhoenixServerApplication.class, args);
 	}
 }
