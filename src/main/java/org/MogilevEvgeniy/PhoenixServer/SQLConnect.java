21c838ddadd5bc44f65781a412ad2083e1966726
NoNaMe273 <mogilevzeka@gmail.com>
2025-09-16 20:32:51 +0300

    formating google style ready

--- src/main/java/org/MogilevEvgeniy/PhoenixServer/SQLConnect.java
+++ src/main/java/org/MogilevEvgeniy/PhoenixServer/SQLConnect.java
@@ -10,109 +10,109 @@ import java.util.ArrayList;
 
 public class SQLConnect {
 
-  public static Connection conn;
-  public static PreparedStatement statmt;
-  public static ResultSet resSet;
+  public static Connection connection;
+  public static PreparedStatement statement;
+  public static ResultSet resultSet;
 
   public static void connection(String dateBaseName) throws ClassNotFoundException, SQLException {
-    conn = null;
+    connection = null;
     Class.forName("org.sqlite.JDBC");
-    conn = DriverManager.getConnection("jdbc:sqlite:src/main/resources/" + dateBaseName + ".s3db");
+    connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/" + dateBaseName + ".s3db");
   }
 
   public static void createDateBase(String request) throws SQLException {
-    statmt = conn.prepareStatement(request);
-    statmt.execute();
+    statement = connection.prepareStatement(request);
+    statement.execute();
   }
 
   public static void writeDateBase(String request) throws SQLException {
-    statmt = conn.prepareStatement(request);
-    statmt.execute();
+    statement = connection.prepareStatement(request);
+    statement.execute();
   }
 
-  public static String gettingFromDateBase(String temp, String dateBaseName, String where,
+  public static String gettingFromDateBase(String temporaryData, String dateBaseName, String where,
       String type) throws SQLException {
-    statmt = conn.prepareStatement("SELECT * FROM " + dateBaseName + " WHERE " + where + " = ?");
-    statmt.setString(1, temp);
-    resSet = statmt.executeQuery();
+    statement = connection.prepareStatement("SELECT * FROM " + dateBaseName + " WHERE " + where + " = ?");
+    statement.setString(1, temporaryData);
+    resultSet = statement.executeQuery();
     String result = "";
-    if (resSet.next()) {
-      result = resSet.getString(type);
+    if (resultSet.next()) {
+      result = resultSet.getString(type);
     }
     return result;
   }
 
   public static int checkDateBase(String login) throws SQLException {
-    statmt = conn.prepareStatement("SELECT * FROM users WHERE login = ?");
-    statmt.setString(1, login);
-    resSet = statmt.executeQuery();
+    statement = connection.prepareStatement("SELECT * FROM users WHERE login = ?");
+    statement.setString(1, login);
+    resultSet = statement.executeQuery();
     int loginInDateBase = 1;
-    if (resSet.next()) {
+    if (resultSet.next()) {
       loginInDateBase = 0;
     }
     return loginInDateBase;
   }
 
-  public static void closeDateBase(int temp) throws SQLException {
-    conn.close();
-    statmt.close();
-      if (temp == 2) {
-          resSet.close();
+  public static void closeDateBase(int temporaryData) throws SQLException {
+    connection.close();
+    statement.close();
+      if (temporaryData == 2) {
+          resultSet.close();
       }
   }
 
   public static int gettingIdDialog(String from, String to) throws SQLException {
-    statmt = conn.prepareStatement(
+    statement = connection.prepareStatement(
         "SELECT * FROM dialogID WHERE first = ? AND second = ? OR second = ? AND first = ?");
-    statmt.setString(1, from);
-    statmt.setString(2, to);
-    statmt.setString(3, from);
-    statmt.setString(4, to);
-    resSet = statmt.executeQuery();
+    statement.setString(1, from);
+    statement.setString(2, to);
+    statement.setString(3, from);
+    statement.setString(4, to);
+    resultSet = statement.executeQuery();
     int id = 0;
-    if (resSet.next()) {
-      id = resSet.getInt("id");
+    if (resultSet.next()) {
+      id = resultSet.getInt("id");
     }
     return id;
   }
 
   public static ArrayList<String> gettingDialogNames(String login) throws SQLException {
     ArrayList<String> tempArray = new ArrayList<>();
-    statmt = conn.prepareStatement("SELECT * FROM dialogID WHERE first = ?");
-    statmt.setString(1, login);
-    resSet = statmt.executeQuery();
-    while (resSet.next()) {
-      tempArray.add(resSet.getString("second"));
+    statement = connection.prepareStatement("SELECT * FROM dialogID WHERE first = ?");
+    statement.setString(1, login);
+    resultSet = statement.executeQuery();
+    while (resultSet.next()) {
+      tempArray.add(resultSet.getString("second"));
     }
-    statmt = conn.prepareStatement("SELECT * FROM dialogID WHERE second = ?");
-    statmt.setString(1, login);
-    resSet = statmt.executeQuery();
-    while (resSet.next()) {
-      tempArray.add(resSet.getString("first"));
+    statement = connection.prepareStatement("SELECT * FROM dialogID WHERE second = ?");
+    statement.setString(1, login);
+    resultSet = statement.executeQuery();
+    while (resultSet.next()) {
+      tempArray.add(resultSet.getString("first"));
     }
     return tempArray;
   }
 
   public static ArrayList<String> gettingDialog(int id) throws SQLException {
     ArrayList<String> tempArray = new ArrayList<>();
-    statmt = conn.prepareStatement("SELECT * FROM dialog WHERE IDDialog = ?");
-    statmt.setInt(1, id);
-    resSet = statmt.executeQuery();
-    while (resSet.next()) {
-      tempArray.add(resSet.getString("datetime"));
-      tempArray.add(resSet.getString("text"));
-      tempArray.add(resSet.getString("from"));
+    statement = connection.prepareStatement("SELECT * FROM dialog WHERE IDDialog = ?");
+    statement.setInt(1, id);
+    resultSet = statement.executeQuery();
+    while (resultSet.next()) {
+      tempArray.add(resultSet.getString("datetime"));
+      tempArray.add(resultSet.getString("text"));
+      tempArray.add(resultSet.getString("from"));
     }
     return tempArray;
   }
 
   public static String gettingKey(String login, String dateBaseName) throws SQLException {
-    statmt = conn.prepareStatement("SELECT * FROM " + dateBaseName + " WHERE login = ?");
-    statmt.setString(1, login);
-    resSet = statmt.executeQuery();
+    statement = connection.prepareStatement("SELECT * FROM " + dateBaseName + " WHERE login = ?");
+    statement.setString(1, login);
+    resultSet = statement.executeQuery();
     String key = "";
-    if (resSet.next()) {
-      key = resSet.getString("key");
+    if (resultSet.next()) {
+      key = resultSet.getString("key");
     }
     return key;
   }
